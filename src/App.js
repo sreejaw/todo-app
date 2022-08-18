3a048d5ad5c32aad4d583c9afada514b0d84d919
sreeja <you@example.com>
2022-08-18 22:10:56 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const [todos,setTodos]=React.useState([]);
+  const [todo,setTodo]=React.useState("");
+  
+
+  
+  function handleSubmit(e){
+    e.preventDefault();
+    
+    const newTodo ={
+      id: new Date().getTime(),
+      text:todo,
+      completed: false
+    }
+    setTodos([...todos].concat(newTodo))
+    setTodo("")
+  }
+  function deleteTodo(id){
+    const updatedTodos = [...todos].filter((todo)=>todo.id !== id)
+
+    setTodos(updatedTodos)
+  }
+  function toggleComplete(id){
+    const updatedTodos=[...todos].map((todo)=>{
+      if(todo.id === id){
+        todo.completed = !todo.completed
+      }
+      return todo
+    })
+
+    setTodos(updatedTodos)
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={(e)=>setTodo(e.target.value)} value={todo}/>
+        <button type="submit">Add ToDo</button>
+      </form>
+      {todos.map((todo)=><div key={todo.id}>
+        <div>{todo.text}</div>
+        <button onClick={()=>deleteTodo(todo.id)}>Delete</button>
+        <input type="checkbox" onChange={()=>toggleComplete(todo.id)} checked={todo.completed} />
+        
+        </div>)}
     </div>
   );
 }
